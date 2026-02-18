3f7f218fce54bfd8589c4d367f587ed015a7be7a
Sahil Singh <sahil171759@gmail.com>
2026-02-18 23:36:40 +0530

    pricing ui created

--- components/layout/pricing-box.tsx
+++ components/layout/pricing-box.tsx
@@ -21,7 +21,7 @@ const PricingBox = () => {
 
   return (
     <div className="flex gap-16">
-      <div className="shadow-xl bg-white px-5 py-6 rounded-4xl z-10 overflow-hidden border-3 border-transparent ">
+      <div className="shadow-xl dark:shadow-neutral-900/10 bg-white dark:bg-black dark:ring-neutral-700/40 dark:ring px-5 py-6 rounded-4xl z-10 overflow-hidden border-3 border-transparent ">
         <div className="flex flex-col items-start gap-2 px- pb-3   ">
           <h2 className="font-bold text-xl">Free</h2>
           <span className="flex items-center gap-2 ">
@@ -54,11 +54,11 @@ const PricingBox = () => {
           </h3>
         </div>
       </div>
-      <div className="shadow-xl bg-white px-5 py-6 rounded-4xl z-10 overflow-hidden border-3 border-transparent ">
+      <div className="shadow-xl bg-white dark:bg-linear-to-bl dark:from-black/95  dark:via-neutral-950 dark:to-black dark:ring-neutral-700/40 dark:ring px-5 py-6 rounded-4xl z-10 overflow-hidden border-3 border-transparent  ">
         <div className="flex flex-col items-start gap-2 px- pb-3 relative ">
-          <div className="bg-blue-400 h-32 w-42 absolute -top-29 right-15 rounded-full blur-2xl "></div>
-          <div className="bg-orange-400 h-10 w-20 absolute bottom-0 -left-10  blur-xl "></div>
-          <div className="bg-orange-400 h-10 w-20 absolute bottom-0 -right-10  blur-xl "></div>
+          <div className="bg-blue-400 dark:bg-blue-400/50 h-32 w-42 absolute -top-29 right-15 rounded-full blur-2xl "></div>
+          <div className="bg-orange-400 dark:bg-orange-400/40 h-10 w-20 absolute bottom-0 -left-10  blur-xl "></div>
+          <div className="bg-orange-400 dark:bg-orange-400/40  h-10 w-20 absolute bottom-0 -right-10  blur-xl "></div>
           <h2 className="font-bold text-xl">Plus</h2>
           <span className="flex items-center gap-2 ">
             <h2 className="font-bold text-5xl">$15</h2>
@@ -67,7 +67,7 @@ const PricingBox = () => {
             </p>
           </span>
           <span className="mt-4 w-full z-20">
-            <Button className="w-full py-5.5 text-base shadow rounded-xl inset-shadow-white bg-neutral-900 inset-shadow-xs ">
+            <Button className="w-full py-5.5 text-base shadow rounded-xl inset-shadow-white dark:inset-shadow-neutral-600 bg-neutral-900 dark:bg-neutral-200 inset-shadow-xs ">
               Get Started
             </Button>
           </span>
