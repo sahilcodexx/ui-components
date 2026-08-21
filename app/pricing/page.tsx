d65bf847e541a40c53f862ecde7d4addd2fdb632
sahilcodexx <sahil171759@gmail.com>
2026-08-21 14:04:13 +0530

    feat: add pricing page and update navigation bar

---
+++ app/pricing/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/common/container";
+import PricingBox from "@/components/layout/pricing-box";
+
+export default function PricingPage() {
+  return (
+    <Container className="flex items-center justify-center">
+      <PricingBox />
+    </Container>
+  );
+}
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -13,28 +13,27 @@ const Navbar = () => {
   const pathname = usePathname();
   const links = [
     { name: "Home", href: "/" },
-    { name: "Convert", href: "/convert" },
     { name: "Pricing", href: "/pricing" },
     { name: "About", href: "/about" },
   ];
 
   return (
     <div className="mt-6 fixed top-6 z-50 m-auto w-full">
-      <div className="flex items-center gap-2 md:gap-5 justify-center dark:bg-black/60 w-fit m-auto  rounded-full px-1.5 py-1.5 border-transparent ring dark:ring-neutral-800/60 ring-neutral-300 bg-white shadow inset-shadow">
-        <div className="flex gap-3 md:gap-8 items-center">
+      <div className="flex items-center gap-2 md:gap-5 justify-center dark:bg-neutral-900 w-fit m-auto  rounded-full px-1.5 py-1.5 border-transparent ring dark:ring-neutral-700/60 ring-neutral-300 bg-white shadow inset-shadow">
+        <div className="flex gap-3 md:gap-1 items-center">
           {links.map((link, index) => (
-            <MotionSpan key={link.href}>
+            <MotionDiv key={link.href}>
               <Link
                 onMouseEnter={() => setHoverd(index)}
                 onMouseLeave={() => setHoverd(null)}
-                className=" relative block group text-center px-2  py-2 md:px-3 md:py-2.5 text-xs md:text-sm"
+                className=" relative block group text-center px-2  py-2 md:px-6 md:py-2.5 text-xs md:text-sm"
                 href={link.href}
               >
                 <AnimatePresence>
                   {(hoverd === index || pathname === link.href) && (
                     <MotionDiv
                       layoutId="hoverd"
-                      className="absolute  bg-black/9 dark:bg-neutral-800/70 inset-0 rounded-full "
+                      className="absolute  bg-black/9 dark:bg-neutral-700/60 inset-0 rounded-full  "
                       transition={{
                         type: "spring",
                         stiffness: 400,
@@ -47,12 +46,12 @@ const Navbar = () => {
                   )}
                 </AnimatePresence>
                 <span
-                  className={`relative z-10 font-medium tracking-wide ${pathname === link.href ? "dark:text-white" : "dark:text-neutral-600 text-neutral-400"} group-hover:text-black dark:group-hover:text-white`}
+                  className={`relative z-10 font-medium tracking-wide ${pathname === link.href ? "dark:text-white" : "dark:text-neutral-400 text-neutral-400"} group-hover:text-black dark:group-hover:text-white`}
                 >
                   {link.name}
                 </span>
               </Link>
-            </MotionSpan>
+            </MotionDiv>
           ))}
         </div>
         <div className="border-l  px-2 md:px-4">
